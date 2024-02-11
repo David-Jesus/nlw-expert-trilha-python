33ae3a9b16b7427fd315022448a2ab9e4dd5990a
David de Jesus Oliveira <67175850+David-Jesus@users.noreply.github.com>
2024-02-10 22:00:38 -0300

    feat: Implementing Error

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,6 +1,20 @@
 from src.views.http_types.http_response import HttpResponse
+from src.errors.error_types.http_unprocessable_entity import HttpUnprocessableEntityError
 
-def handle_errors(error: Exception) -> HttpResponse: 
+def handle_errors(error: Exception) -> HttpResponse:
+    if isinstance(error, HttpUnprocessableEntityError):
+        # Enviar para um log ou Email
+        return HttpResponse(
+            status_code=error.status_code,
+            headers={},
+            body={
+                'errors': [{
+                    "title": error.name,
+                    "detail": error.message
+                }]
+            }
+        )
+    
     return HttpResponse(
         status_code=500,
         headers={},
--- src/main/routes/tag_routes.py
+++ src/main/routes/tag_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify
 from src.views.http_types.http_request import HttpRequest
 from src.views.tag_creator_view import TagCreatorView
 from src.errors.error_handler import handle_errors
+from src.validators.tag_creator_validator import tag_creator_validator
 
 tags_routes_bp = Blueprint('tags_routes', __name__)
 
@@ -10,6 +11,7 @@ def create_tags():
     response = None
     
     try:
+        tag_creator_validator(request)
         tag_creator_view = TagCreatorView()
 
         http_request = HttpRequest(body=request.json)
